72486dae728a5a3897e2702d5c519fe8a8878845
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-01-07 07:44:20 -0500

    label component testing 100%

--- test/label.test.js
+++ test/label.test.js
@@ -1,11 +1,12 @@
 import { expect, fixture, html } from '@open-wc/testing';
-import { LabelElement } from '../components/label/label.js';
+import { LabelElement } from '../components/header/label/label.js';
 // import component for testing
 
 describe('my-test', () => {
   it('works', async () => {
-    const el = await fixture(html` <label for="${ this.label }">${ this.label }</label> `);
-    expect(el).dom.to.equal('<label for="${ this.label }">${ this.label }</label>')
+    const el = await fixture(html` <label-element for="${ 'vegetable' }"> ${ 'vegetable' } </label-element> `);
+    expect(el.getAttribute('for')).to.equal('vegetable');
+    // expect(el).dom.to.equal('<label-element for="vegetable"> vegetable </label-element>')
   });
 });
 
