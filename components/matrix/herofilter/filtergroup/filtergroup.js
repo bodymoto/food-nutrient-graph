25d50cb405dd1345e2de92fdd1932923cffb673f
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-08 02:25:01 -0500

    new component added for site header, edited name of previous header component

---
+++ components/matrix/herofilter/filtergroup/filtergroup.js
@@ -0,0 +1,70 @@
+import { LitElement, html, css } from 'lit';
+
+export class FilterGroupElement extends LitElement {
+	
+	static properties = {
+		checked: { type: Boolean },
+		label: { type: String },
+	}
+
+	constructor() {
+		super();
+		this.checked = false;
+		this.label = '';
+	}
+
+	static styles = css`
+		:host {
+			width: 200px;
+			height: 50px;
+		}
+		label {
+			border: 2px solid black;
+			border-radius: 10px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			user-select: none;
+			width: 100%;
+			height: 100%;
+			cursor: pointer;
+			font-family: "Gill Sans", sans-serif;
+			letter-spacing: 1px;
+			font-size: 24px;
+			transition: .5s;
+			text-transform: capitalize;
+		}
+		input[type="checkbox"] {
+			display: none;
+		}
+		input[type="checkbox"]:checked + label {
+			background-color: orange;
+		}
+	`;
+
+	render() {
+		return html`
+			<input @change=${this.handleChecked} type="checkbox" for=${this.label}>
+			<label @click=${this.handleClick} for=${this.label}>${this.label}</label>
+		`;
+	}
+
+	handleClick() {
+		this.shadowRoot.querySelector('input').click();
+	}
+
+	async handleChecked(event) {
+		this.checked = event.target.checked;
+		
+		const options = {
+			detail: {	filter: this },
+			bubbles: true,
+			composed: true
+		};
+
+    await this.updateComplete;
+		this.dispatchEvent(new CustomEvent('groups-event', options));
+	}
+};
+
+customElements.define('filter-group-element', FilterGroupElement);
---
+++ components/matrix/herofilter/herofilter.js
@@ -0,0 +1,85 @@
+import { LitElement, html, css } from 'lit';
+import { FilterGroupElement } from './filtergroup/filtergroup.js';
+
+export class HeroFilterElement extends LitElement {
+
+	static get properties() {
+		return {
+			data: { type: Array },
+			foodGroups: { type: Array },
+			checked: { type: Object },
+			filteredData: { type: Array }
+		};
+	}
+
+	constructor() {
+		super();
+
+		this.data = [];
+		this.foodGroups = [];
+		this.checked = {};
+		this.filteredData = [];
+
+		this.addEventListener('groups-event', (event) => {
+
+			const category = event.detail.filter.label; // 'vegetable'
+			const selected = event.detail.filter.checked; // true
+
+			this.checked[category] = selected; // { vegetable: true, dairy: false }
+			this.filtered();
+		});
+	}
+
+	async filtered() {
+		this.filteredData = this.data.filter(item => this.checked[item['Group']]);
+
+		const options = {
+			detail: {
+				filteredData: this.filteredData,
+			},
+			bubbles: true,
+			composed: true
+		};
+
+    await this.updateComplete;
+		this.dispatchEvent(new CustomEvent('filter-data', options));
+	}
+
+	willUpdate(changedProperties) {
+		if (changedProperties.has('foodGroups')){
+			this.foodGroups = this.getFoodGroups();
+		}
+	}
+
+	getFoodGroups() {
+		let foods = {};
+		this.data.forEach((item) => foods[item['Group']] = true);
+		return Object.keys(foods).sort();
+	}
+
+	static styles = css`
+		:host {
+			margin: 20px;
+			height: 200px;
+			display: flex;
+			flex-wrap: wrap;
+			gap: 10px;
+			align-content: center;
+			justify-content: center;
+		}
+	`;
+
+	render() {
+		return html`
+			${
+				this.foodGroups.map((item) => {
+					return html`
+					<filter-group-element label=${item}></filter-group-element>
+					`
+				}
+			)}
+		`;
+	}
+};
+
+customElements.define('hero-filter-element', HeroFilterElement);
