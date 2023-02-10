5e0013f530c7591549cec01b0689051c0e0a1a26
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-10 09:06:13 -0500

    import new article component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { MatrixElement } from '../components/matrix/matrix.js';
 import { HeaderElement } from '../components/header/header.js';
+import { ArticleElement } from '../components/article/article.js';
 import { data } from '../data.js';
 
 const header = document.createElement('header-element');
 const matrix = document.createElement('matrix-element');
+const article = document.createElement('article-element');
+
 matrix.data = data;
+
 document.body.appendChild(header);
 document.body.appendChild(matrix);
+document.body.appendChild(article);
 
 // Your render() method should follow these guidelines:
 // Avoid changing the component's state
