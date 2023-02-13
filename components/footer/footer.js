667526e15d20216f4c9065c28be21d55597229ca
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-13 04:37:18 -0500

    new component + image

---
+++ components/footer/footer.js
@@ -0,0 +1,15 @@
+import { LitElement, html, css } from 'lit';
+
+export class FooterElement extends LitElement {
+	static properties = {}
+
+	constructor() { super(); }
+
+	static styles = css``;
+
+	render() {
+		return html``;
+	}
+};
+
+customElements.define('footer-element', FooterElement);
