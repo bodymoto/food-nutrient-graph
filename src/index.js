4d679bbde55562b25e29d8e5c5588c08471476b9
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-13 04:36:40 -0500

    update to add footer component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import { MatrixElement } from '../components/matrix/matrix.js';
 import { HeaderElement } from '../components/header/header.js';
 import { ArticleElement } from '../components/article/article.js';
+import { FooterElement } from '../components/footer/footer.js';
 import { data } from '../data.js';
 
 const header = document.createElement('header-element');
 const matrix = document.createElement('matrix-element');
 const article = document.createElement('article-element');
+const footer = document.createElement('footer-element');
 
 matrix.data = data;
 
 document.body.appendChild(header);
 document.body.appendChild(matrix);
 document.body.appendChild(article);
+document.body.appendChild(footer);
 
 // Your render() method should follow these guidelines:
 // Avoid changing the component's state
